8342f4c355f0c4dfb144c7d121179802396bc452
SDemirhan <se.demirhann@gmail.com>
2022-08-13 22:05:55 +0300

    3.C

--- src/test/java/Day1Git.java
+++ src/test/java/Day1Git.java
@@ -1,6 +1,7 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1.denemesi");
-        System.out.println("Git 1.denemesi");
+        System.out.println("Git 2.denemesi");
+        System.out.println("Git 3.denemesi");
     }
 }
