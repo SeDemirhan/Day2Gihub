14517cc013c2c0895be394777dbd0a4e152e91cc
SDemirhan <se.demirhann@gmail.com>
2022-08-13 23:14:38 +0300

    yeni class

--- src/test/java/Day1Git.java
+++ src/test/java/Day1Git.java
@@ -1,6 +1,8 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1.denemesi");
-        System.out.println("Git 1.denemesi");
+        System.out.println("Git 2.denemesi");
+        System.out.println("Git 3.denemesi");
+        System.out.println("Git 4.denemesi");
     }
 }
